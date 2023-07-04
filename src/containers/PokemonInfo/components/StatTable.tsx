661f0d48b6de98c22761c7f96d8b4949f9aa03c1
unknown <mauricio.borawski@gmail.com>
2023-07-03 21:22:11 -0300

    Improve import on StatTable

--- src/containers/PokemonInfo/components/StatTable.tsx
+++ src/containers/PokemonInfo/components/StatTable.tsx
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { PokemonStats } from "../../../types";
+import { PokemonStats } from "@/types";
 
 export const StatTable: FunctionComponent<{
   stats: Array<PokemonStats>;
